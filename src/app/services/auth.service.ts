72ebf9b2e9d48a0935ec64e72e7248ff14a6a83d
julienudev <julien.udev@gmail.com>
2020-01-29 21:47:13 +0100

    clean service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,5 @@
-import { HttpHeaders, HttpClient } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
 @Injectable({
   providedIn: "root"
 })
@@ -9,59 +8,16 @@ export class AuthService {
   public password: string;
 
   constructor(private http: HttpClient) {}
+
   url: string = "http://92.222.69.104:80/todo/listes/";
   objet: any;
 
   login(httpOptions) {
-    // const httpOptions = {
-    //   headers: new HttpHeaders({
-    //     username: "julien",
-    //     password: "julien"
-    //   })
-    // };
-
     let url: string = "http://92.222.69.104:80/todo/listes/";
 
     this.http.get(url, httpOptions).subscribe(res => {
-      //console.log(data);
       this.objet = res;
       console.log("retour du http dans service" + this.objet);
     });
   }
-
-  // login(username: string, password: string) {
-  //   const httpOptions = {
-  //     headers: new HttpHeaders({
-  //       username: "julien",
-  //       password: "julien"
-  //     })
-  //   };
-
-  //   let url: string = "http://92.222.69.104:80/todo/listes/";
-
-  //   this.http
-  //     .get(url, httpOptions)
-  //     .subscribe(res => console.log("reponse du http :" + res));
-  // }
 }
-
-//   getData(data: Data): Data[] {
-//     const httpOptions = {
-//       headers: new HttpHeaders({
-//         login: "julien",
-//         password: "julien"
-//       })
-//     };
-
-//     let url: string = "http://92.222.69.104:80/todo/listes/";
-
-//     this.http.get(url, httpOptions).subscribe(
-//       res => console.log("reponse du http :" + res),
-//       msg => console.log(msg)
-//     );
-
-//     //console.log("DATAS AVEC:" + DATAS);
-
-//     return;
-//   }
-// }
